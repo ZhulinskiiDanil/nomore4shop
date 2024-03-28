b2d445ec24df716aca20d66ea5b7d2bfa6d9d072
ZhulinskiiDanil <zhulinskiidanil@gmail.com>
2024-03-28 12:08:25 +0100

    chore: add user info to header (avatar, balance)

--- src/components/Widgets/Header/Header.vue
+++ src/components/Widgets/Header/Header.vue
@@ -1,5 +1,5 @@
 <template>
-  <header :class="$style.header">
+  <header v-if="profile" :class="$style.header">
     <NuxtLink to="/market">
       <div :class="$style.company">
         <div :class="$style.shadow"></div>
@@ -14,18 +14,29 @@
     </NuxtLink>
     <NuxtLink to="/profile">
       <div :class="$style.user">
-        <div :class="$style.balance">0 USDT</div>
+        <div :class="$style.balance">
+          {{ formatBalance(profile.balance) }}
+        </div>
         <div :class="$style.avatar">
-          <img
-            src="https://friendfunction.ru/upload/iblock/070/z6y5xr0kqlyb7td44tflvjh270yl009g/shapka_carhartt_wip_blizzard_beanie_ash_heather_1.jpg"
-            alt="Avatar"
-          />
+          <img :src="profile.avatar || ''" alt="Avatar" />
         </div>
       </div>
     </NuxtLink>
   </header>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+const { profile } = useProfile();
+
+const formatBalance = (n: number) => {
+  return n.toLocaleString('ru', {
+    style: 'currency',
+    currency: 'USD',
+    minimumFractionDigits: 2,
+    maximumFractionDigits: 2,
+    currencyDisplay: 'code'
+  });
+};
+</script>
 
 <style lang="scss" src="./Header.module.scss" module></style>
